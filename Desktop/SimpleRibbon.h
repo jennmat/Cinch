aac8f42ce3d607b8c159d8d63c99852f70a3af7b
Matt Jennings <mjennings@state.nv.us>
2013-08-14 16:28:20 -0700

    Include the ribbon rc and resource top level

---
+++ Desktop/SimpleRibbon.h
@@ -0,0 +1,43 @@
+// *****************************************************************************
+// * This is an automatically generated header file for UI Element definition  *
+// * resource symbols and values. Please do not modify manually.               *
+// *****************************************************************************
+
+#pragma once
+
+#define cmdTabHome 2 
+#define cmdTabHome_LabelTitle_RESID 200
+#define cmdTabUsers 3 
+#define cmdTabUsers_LabelTitle_RESID 201
+#define cmdTabDebug 4 
+#define cmdTabDebug_LabelTitle_RESID 202
+#define cmdShapesGroup 5 
+#define cmdShapesGroup_LabelTitle_RESID 210
+#define cmdDebugGroup 6 
+#define cmdDebugGroup_LabelTitle_RESID 213
+#define cmdViewGroup 7 
+#define cmdViewGroup_LabelTitle_RESID 211
+#define cmdPerspectivesGroup 8 
+#define cmdPerspectivesGroup_LabelTitle_RESID 60001
+#define IDR_CMD_DEBUGOUTPUT 9 
+#define IDR_CMD_DEBUGOUTPUT_LabelTitle_RESID 212
+#define IDR_CMD_SIZEANDCOLOR 10 
+#define IDR_CMD_SIZEANDCOLOR_LabelTitle_RESID 250
+#define IDR_CMD_SIZEANDCOLOR_LargeImages_RESID 291
+#define IDR_CMD_SWITCHPERSPECTIVE 11 
+#define IDR_CMD_DELETE 12 
+#define IDR_CMD_DELETE_LabelTitle_RESID 251
+#define IDR_CMD_DELETE_LargeImages_RESID 292
+#define IDR_CMD_NEW_VIEW 13 
+#define IDR_CMD_NEW_VIEW_LabelTitle_RESID 252
+#define IDR_CMD_NEW_VIEW_LargeImages_RESID 293
+#define IDR_CMD_NEW_PERSPECTIVE 14 
+#define IDR_CMD_NEW_PERSPECTIVE_LabelTitle_RESID 253
+#define IDR_CMD_NEW_PERSPECTIVE_LargeImages_RESID 295
+#define cmdQat 15 
+#define cmdFileMenu 16 
+#define cmdMRUList 17 
+#define cmdMRUList_LabelTitle_RESID 280
+#define IDC_CMD_EXIT 18 
+#define IDC_CMD_EXIT_LabelTitle_RESID 290
+#define IDC_CMD_EXIT_LargeImages_RESID 294
