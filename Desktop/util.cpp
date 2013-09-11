00b33b8c0ba223ea720c7d47e8f2db5e6ae26b8f
Matt Jennings <mjennings@state.nv.us>
2013-09-11 13:21:58 -0700

    Use utf8 encoding

--- Desktop/util.cpp
+++ Desktop/util.cpp
@@ -7,9 +7,9 @@ wstring s2ws(const string& s)
 {
     int len;
     int slength = (int)s.length() + 1;
-    len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0); 
+    len = MultiByteToWideChar(CP_UTF8, 0, s.c_str(), slength, 0, 0); 
     wchar_t* buf = new wchar_t[len];
-    MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
+    MultiByteToWideChar(CP_UTF8, 0, s.c_str(), slength, buf, len);
     wstring r(buf);
     delete[] buf;
     return r;
@@ -19,9 +19,9 @@ std::string ws2s(const std::wstring& s)
 {
     int len;
     int slength = (int)s.length() + 1;
-    len = WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, 0, 0, 0, 0); 
+    len = WideCharToMultiByte(CP_UTF8, 0, s.c_str(), slength, 0, 0, 0, 0); 
     char* buf = new char[len];
-    WideCharToMultiByte(CP_ACP, 0, s.c_str(), slength, buf, len, 0, 0); 
+	WideCharToMultiByte(CP_UTF8, 0, s.c_str(), slength, buf, len, 0, 0); 
     std::string r(buf);
     delete[] buf;
     return r;
